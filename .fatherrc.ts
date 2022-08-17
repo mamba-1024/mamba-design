9ab8b9c3617c118e321c8706c6586c51b28ce797
huangketong <ketong.hkt@alibaba-inc.com>
2022-08-17 17:55:52 +0800

    chore: father

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,5 +1,7 @@
 export default {
+  // https://github.com/umijs/father
   // more father 4 config: https://github.com/umijs/father-next/blob/master/docs/config.md
-  esm: 'rollup',
-  cjs: 'rollup',
+  esm: {},
+  cjs: {},
+  umd: {},
 };
